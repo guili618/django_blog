03ecdfc15faae4b2ab910c6853067adf1b4f42bb
guiaixiao <824541050@qq.com>
2019-03-17 21:19:17 +0800

    cbv crud

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+#from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
@@ -11,4 +13,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,37 @@
 from django.conf.urls import url
 from . import  views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 
 urlpatterns = [
-    url(r'^$', views.home,name='blog-home'),
+    #url(r'^$', views.home,name='blog-home'),
+    url(r'^$', PostListView.as_view(),name='blog-home'),
+    url(r'^post/(?P<pk>[0-9]+)/$', PostDetailView.as_view(),name='post-detail'),
+    url(r'post/add/$', PostCreateView.as_view(), name='post-add'),
+    url(r'post/(?P<pk>[0-9]+)/update$', PostUpdateView.as_view(),\
+                                                            name='post-update'),
+    url(r'post/(?P<pk>[0-9]+)/delete/$', PostDeleteView.as_view(),\
+                                                            name='post-delete'),
     url(r'^about/', views.about,name='blog-about'),
 ]
+'''
+urlpatterns = [
+    url(r'^$', PostListView.as_view(),name='blog-home'),
+    url(r'^user/(?P<username>\w+)/$', UserPostListView.as_view(),name='user-posts'),
+    
+    
+    #url(r'^$', views.home,name='blog-home'),
+    url(r'^about/', views.about,name='blog-about'),
+]
+
+
+
+'''
+
+
+# <app>/<model>_<view_type>.html
